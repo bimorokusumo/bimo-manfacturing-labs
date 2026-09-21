82648a0a4da55d1309a88545c097fae7051e05fe
bimorokusumo <bimorokusumo@users.noreply.github.com>
2026-09-21 12:39:39 +0000

    deploy: 9b8e4b0a4aa7ee87d80c217fce26bc02ac71d3a4

--- assets/TeacherGradebook-Q77-5wcQ.js
+++ assets/TeacherGradebook-Q77-5wcQ.js
@@ -1,4 +1,4 @@
-import{r as e}from"./rolldown-runtime-hePW80VL.js";import{s as t}from"./vendor-react-CrESwwka.js";import{c as n}from"./vendor-libs-CzXoy4to.js";import{a as r,c as i,d as a,f as o,i as s,n as c,o as l,r as u,s as d,u as f}from"./index-BQJkkHMS.js";var p=e(n(),1),m=t(),h=`function doPost(e) {
+import{r as e}from"./rolldown-runtime-hePW80VL.js";import{s as t}from"./vendor-react-CrESwwka.js";import{c as n}from"./vendor-libs-CzXoy4to.js";import{a as r,c as i,d as a,f as o,i as s,n as c,o as l,r as u,s as d,u as f}from"./index-BZFqJkK8.js";var p=e(n(),1),m=t(),h=`function doPost(e) {
   var lock = LockService.getScriptLock();
   lock.tryLock(30000);
 
